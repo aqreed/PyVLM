590c3cfc71a83b30f8578ed1f73f79b131b73e25
Álex Sáez <alex.saez.12@gmail.com>
2016-10-17 19:43:57 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-"""
-Python Flight Mechanics Engine (PyFME).
-Copyright (c) AeroPython Development Team.
-Distributed under the terms of the MIT License.
+from setuptools import setup, find_packages
 
-Title
------
-
-"""
+setup(
+    name="PyVLM",
+    version="0.0.dev0",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['numpy', 'matplotlib'],
+    tests_requires=['pytest']
+    )
