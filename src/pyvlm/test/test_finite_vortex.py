d4f1dfdd3a078fbe22f88fae501e8684380cdfaa
aneqr <andres.quezada.reed@gmail.com>
2016-10-21 02:00:05 +0200

    gamma=4pi in test, bibliography

--- src/pyvlm/test/test_finite_vortex.py
+++ src/pyvlm/test/test_finite_vortex.py
@@ -3,10 +3,19 @@
     of the function that calculates the velocity induced by a
                               straight line finite vortex: it
     Y			+ P(x, y)	  must be infinite if P approaches
-     ^			|			  OX at 0 < x < 1 and zero if x<1
+     ^			|			  OX at 0 < x < 1 and zero if x < 1
      |			|
-     +=======+--------> X
+     +=======+--+-----> X
     P1(0,0)  P2(0,1)
+
+    Results can be compared to those obtained in the bibliography:
+
+    [1] Cummings, R.M., Mason, W.H., Morton, S.A., McDaniel, D.R.,
+        "Applied Computational Aerodynamics", Cambridge University
+        Press, 1998, Chapter 6 - 31
+    [2] Gandía, F., Gonzalo, J., Margot, X., Meseguer Ruiz, J.,
+        "Fundamentos de los Métodos Numéricos en Aerodinámica",
+        Garceta, 2013, pp. 100
 """
 
 import numpy as np
@@ -17,23 +26,23 @@ from pyvlm.vortices import v_induced_by_finite_vortex_line
 P1, P2 = np.array([0, 0]), np.array([1, 0])
 
 plt.style.use('ggplot')
-
-for x in np.linspace(0.75, 1.25, 5):
-    y_plot = np.zeros(40)
-    v_plot = np.zeros(40)
+n = 30  # number of y-points
+for x in (0.5, 1.5, 2.0, 3.0):
+    y_plot = np.zeros(n)
+    v_plot = np.zeros(n)
 
     i = 0
-    for y in np.linspace(0.0001, 2, 40):
+    for y in np.linspace(0.0001, 2, n):
         P = np.array([x, y])
-        w = v_induced_by_finite_vortex_line(P, P1, P2)
+        v = v_induced_by_finite_vortex_line(P, P1, P2, gamma=4*np.pi)
         y_plot[i] = y
-        v_plot[i] = abs(w)
+        v_plot[i] = abs(v)
         i += 1
 
     plt.plot(y_plot, v_plot, label='x/l= %s' % x)
 
 plt.xlabel('y/l')
 plt.ylabel('induced velocity')
-plt.ylim(0, 0.25)
+plt.ylim(0, 1.0)
 plt.legend(loc='upper right', shadow=True)
 plt.show()
