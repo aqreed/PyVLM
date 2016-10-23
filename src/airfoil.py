05275ecd3c4c61e94f958e51bf5e59b31a830a8b
aneqr <andres.quezada.reed@gmail.com>
2016-10-24 01:58:28 +0200

    added documentation to airfoils

--- src/airfoil.py
+++ src/airfoil.py
@@ -3,6 +3,27 @@ import matplotlib.pyplot as plt
 
 
 def camber_gradient_NACA4(x, M=2, P=4):
+    """
+    Camber line gradient (dz/dx) for 4-digit NACA airfoils.
+    Default values for NACA 2412
+
+    Reference:
+    [1] http://airfoiltools.com/airfoil/naca4digit
+
+    Parameters
+    ----------
+    x : float
+        Chord position, from 0 to 1
+    M, P : integer
+           First and second digit on the NACA designation
+           M - maximum camber (divided by 100)
+           P - maximum camber position (divided by 10)
+
+    Returns
+    -------
+    dz : float
+         Camber gradient
+    """
     M /= 100
     P /= 10
     if x < P:
