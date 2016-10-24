af0e503f5e8b236998e93f0a9c428be718f567bc
aneqr <andres.quezada.reed@gmail.com>
2016-10-24 03:34:21 +0200

    added test for airfoil

---
+++ src/pyvlm/test/test_airfoil.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pyvlm.airfoil import camber_gradient_NACA4
+
+# Plotting
+plt.style.use('ggplot')
+for x in np.linspace(0, 1, 21):
+    dz = camber_gradient_NACA4(x)
+    plt.plot(x, dz, 'ro')
+    print(np.rad2deg(dz))
+plt.show()
