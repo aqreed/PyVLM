d04c617d927f2c28b9a7f23258197ee79e29488c
aneqr <andres.quezada.reed@gmail.com>
2016-10-25 18:01:16 +0200

    adapted example to new vlm class

--- src/example_pyvlm_class.py
+++ src/example_pyvlm_class.py
@@ -8,29 +8,32 @@ from pyvlm.airfoils import camber_gradient_NACA4
 
 V = 20.0
 alpha = np.deg2rad(3)
+
 c = 4  # panel chord length
 b = 10  # panel span length
+
 n = 5  # number of panels chordwise
-m = 2  # number of panels spanwise
+m = 3  # number of panels spanwise
 A = np.array([c, -b/2])
-B = np.array([0, -b/10])
-C = np.array([0, b/10])
+B = np.array([0, -b/5])
+C = np.array([0, b/5])
 D = np.array([c, b/2])
 
-leading_edges_coord = [B, C]
-chord_lengths = [c, c]
-#leading_edges_coord = [A, B, C, D]
-#chord_lengths = [c/4, c, c, c/4]
+#leading_edges_coord = [B, C]
+#chord_lengths = [c, c]
+leading_edges_coord = [A, B, C, D]
+chord_lengths = [c/4, c, c, c/4]
 
-wing = PyVLM(leading_edges_coord, chord_lengths, V, alpha, n, m)
+example = PyVLM(V, alpha)
 
-Y, A, X = wing.get_linear_eq()
-Points, Panels, Chord_pos = wing.get_geometry()
+Points, Panels, Chords, Chordwise_panel_pos = example.create_geometry(
+                                              leading_edges_coord,
+                                              chord_lengths, n, m)
+
+Y, A, X = example.vlm()
+
 
 np.set_printoptions(precision=3)
-print('\n', 'Matrix A =', '\n', A, '\n')
-print('Y =', Y, '\n')
-print('X =', X, '\n')
 
 # Plotting
 print('\n', 'Nº of points = ', len(Points))
@@ -40,6 +43,10 @@ for i in range(0, len(Points)):
 print('\n', 'Nº of Panels = ', len(Panels))
 for i in range(0, len(Panels)):
     print('Panel %s' % i, Panels[i])
+
+print('\n', 'Matrix A =', '\n', A, '\n')
+print('Y =', Y, '\n')
+print('X =', X, '\n')
 
 plt.style.use('ggplot')
 for i in range(0, len(Points)):
