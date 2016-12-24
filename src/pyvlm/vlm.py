7f915e772d0d5e6850c3bfa2dd55a543560d5cab
aneqr <andres.quezada.reed@gmail.com>
2016-12-24 18:03:01 +0100

    edited vlm to accomodate new panel class. Clarity

--- src/pyvlm/vlm.py
+++ src/pyvlm/vlm.py
@@ -9,7 +9,7 @@ from .airfoils import NACA4
 class PyVLM(object):
     """
     Given the geometry, angle of attack, upstream velocity and
-    mesh chordwise and spanwise density, applies the VLM theory
+    mesh chordwise and spanwise densities, applies the VLM theory
     to the surface.
 
     Parameters
@@ -34,43 +34,57 @@ class PyVLM(object):
         self.alpha = alpha
 
         self.Points = []
-        self.Panel_points = []
-        self.Chordwise_panel_position = []
+        self.Panels_points = []
+        self.Panels_span = []
+        self.Chordwise_panel_positions = []
 
     def add_geometry(self, lead_edge_coord, chord_lengths, n, m):
         """ Allows to add wings, stabilizers, canard wings or any
-            other lifting surface to the mesh, defined by its chords
-            location (leading edge) and length. Also the density of
-            the mesh can be controlled by n and m """
+            other lifting surface to the mesh, defined by given
+            chords. These are defined by their leading edges
+            locations and their lengths. Also the density of the
+            mesh can be controlled spanwise and chordwise with
+            n and m """
 
         if len(lead_edge_coord) != len(chord_lengths):
-            msg = 'Same number of chords and l.e. required'
+            msg = 'Same number of chords and leading edges required'
             raise ValueError(msg)
 
         # MESH GENERATION
+
+        # When more than two chords -with their respectives leading
+        # edges coordinates- are provided, it iterates through the
+        # list containing both location and length
+
         Nle = len(lead_edge_coord)
 
         for k in range(0, Nle - 1):
-            # When more than two chords are provided, it iterates
-            # through the list containing both location and length
-
             leading_edges = [lead_edge_coord[k],
                              lead_edge_coord[k + 1]]
             chords = [chord_lengths[k],
                       chord_lengths[k + 1]]
 
+            # The mesh is created taking into account the desired
+            # mesh density spanwise -"n"- and chordwise -"m"-
+
             mesh = Mesh(leading_edges, chords, n, m)
 
+            # The points of the mesh, its panels - sets of 4 points
+            # orderly arranged -, the position of each panel relative to
+            # its local chord and the span of each panel are calculated
+
             Points_ = mesh.points()
-            Panel_points_ = mesh.panel()
-            Chordwise_panel_position_ = mesh.panel_chord_position()
+            Panels_points_ = mesh.panels()
+            Panels_span_ = mesh.panels_span()
+            Chordwise_panel_positions_ = mesh.panel_chord_positions()
 
             self.Points.extend(Points_)
-            self.Panel_points.extend(Panel_points_)
-            self.Chordwise_panel_position.extend(Chordwise_panel_position_)
+            self.Panels_points.extend(Panels_points_)
+            self.Panels_span.extend(Panels_span_)
+            self.Chordwise_panel_positions.extend(Chordwise_panel_positions_)
 
-        return (self.Points, self.Panel_points,
-                self.Chordwise_panel_position)
+        return (self.Points, self.Panels_points, self.Panels_span,
+                self.Chordwise_panel_positions)
 
     def vlm(self):
         """ For a given set of panels (defined by its 4 points) and
@@ -81,39 +95,56 @@ class PyVLM(object):
             condition will be imposed. Computes the circulation by
             solving the linear equation """
 
-        Panel_points = self.Panel_points
-        Chordwise_panel_position = self.Chordwise_panel_position
+        Panels_points = self.Panels_points
+        Chordwise_panel_positions = self.Chordwise_panel_positions
 
         V = self.V
         alpha = self.alpha
 
-        # INDUCED VELOCITIES
-        N = len(Panel_points)
+        # 1. BOUNDARY CONDITION
+        # To impose the boundary condition we have to calculate
+        # the normal components of (a) induced velocities Wn and
+        # (b) upstream velocity Vn_inf
+
+        #     1.a INDUCED VELOCITIES
+        #     stored in the matrix "A" where the element Aij is
+        #     the velocity induced by the panel j on the panel i
+
+        N = len(Panels_points)
         A = np.zeros(shape=(N, N))
 
         for i in range(0, N):
-            P1, P2, P3, P4 = Panel_points[i][:]
+            P1, P2, P3, P4 = Panels_points[i][:]
             panel_pivot = Panel(P1, P2, P3, P4)
             s = panel_pivot.area()
             CP = panel_pivot.control_point()
 
             for j in range(0, N):
-                PP1, PP2, PP3, PP4 = Panel_points[j][:]
+                PP1, PP2, PP3, PP4 = Panels_points[j][:]
                 panel = Panel(PP1, PP2, PP3, PP4)
-                w = panel.induced_velocity(CP)
-                A[i, j] = w
+                Wn = panel.induced_velocity(CP)
+                A[i, j] = Wn
 
-        # CIRCULATION by solving the linear equation (AX = Y)
-        Y = np.zeros(shape=N)
+        #     1.b UPSTREAM VELOCITIES
+        #     that will depend on the angle of attach -"alpha"-
+        #     and the camber gradient at each panel's position
+        #     within the local chord
+
+        Vinf_n = np.zeros(shape=N)
 
         airfoil = NACA4()
         for i in range(0, N):
-            Y[i] = alpha - airfoil.camber_gradient(Chordwise_panel_position[i])
-            Y[i] *= -self.V
+            position = Chordwise_panel_positions[i]
+            Vinf_n[i] = alpha - airfoil.camber_gradient(position)
+            Vinf_n[i] *= -self.V
 
-        X = np.linalg.solve(A, Y)
+        # 2. CIRCULATION (gamma)
+        # by solving the linear equation (AX = Y) where X = gamma
+        # and Y = V_inf
 
-        return Y, A, X
+        gamma = np.linalg.solve(A, Vinf_n)
+
+        return Vinf_n, A, gamma
 
 
 # gamma_plot = X
