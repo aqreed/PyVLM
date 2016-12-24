dbc03e9fd3c9f032dd72c0e085a15388fdadb98d
aneqr <andres.quezada.reed@gmail.com>
2016-12-24 17:59:45 +0100

    edited panel for clarity

--- src/pyvlm/panel.py
+++ src/pyvlm/panel.py
@@ -8,10 +8,10 @@ from .vortices import (vortex_position_in_panel,
 class Panel(object):
     """
            y ^
-             |              Points defining the panel are
-        P3-C-|-D-P4         named clockwise. Points defining
-         | | | |  |         the horseshoe are named clockwise
-         | | +-P--|--->     as well.
+             |              The panel is defined by the (x, y) coordinates
+        P3-C-|-D-P4         of four points ordered clockwise - namely P1,
+         | | | |  |         P3, P3 and P4 -. Points defining the horseshoe
+         | | +-P--|--->     - A, B, C and D - are named clockwise as well.
          | |   |  |   x
         P2-B---A-P1
 
@@ -26,19 +26,41 @@ class Panel(object):
         self.P2 = P2
         self.P3 = P3
         self.P4 = P4
-        self.CP = 0  # control point
-        self.w = 0  # induced velocity
+
+        P1P2 = self.P2 - self.P1
+        P3P4 = self.P4 - self.P3
+
+        if np.cross(P1P2, P3P4) != 0:
+            msg = 'Panel incorrectly defined, P1P2 and P3P4 not parallel'
+            raise ValueError(msg)
 
     def area(self):
-        """ Yields the area calculated with the points provided
-            as arguments """
+        """ Yields the area calculated with the points provided as
+            arguments """
 
         return area_4points(self.P1, self.P2, self.P3, self.P4)
 
+    def span(self):
+        """ Yields the span of the panel, which must be constant - P1P2
+            and P3P4 must be parallel - """
+
+        b = self.P3[1] - self.P2[1]
+        b__ = self.P4[1] - self.P1[1]
+
+        if abs(b) != abs(b__):
+            msg = 'Panel incorrectly defined, |P1P4| =/ |P2P3|'
+            raise ValueError(msg)
+
+        return abs(b)
+
     def _vortex_position(self):
-        """ Returns the points that define the position of the
-            vortex horseshoe within the panel, called ABCD in
-            documentation above plus the control point """
+        """ Returns the (x, y) coordinates of the four points that define
+            the position of the vortex horseshoe within the panel plus
+            the control point, identified as PABCD in the documentation
+            above.
+
+                *Remainder: the coordinates are returned as a list,
+                            following the order [P, A, B, C, D] """
 
         points_of_the_vortex = vortex_position_in_panel(self.P1,
                                                         self.P2,
@@ -48,16 +70,15 @@ class Panel(object):
         return points_of_the_vortex
 
     def control_point(self):
-        """ Returns the position of the control point """
+        """ Returns the (x, y) coordinates position of the control point """
 
         control_point_position = self._vortex_position()[0]
 
         return control_point_position
 
     def induced_velocity(self, control_point_pos):
-        """ Calculates and returns the induced velocity by a
-            horseshoe vortex on a control point, defined as
-            argument of the method """
+        """ Calculates and returns the induced velocity by a horseshoe
+            vortex on a control point, defined as argument of the method """
 
         _points_vortex = self._vortex_position()
         v = v_induced_by_horseshoe_vortex(control_point_pos,
