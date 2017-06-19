6478668b3bcafb25664a218984959210b361449d
aneqr <andres.quezada.reed@gmail.com>
2017-06-19 22:37:03 +0200

    Pilatus PC12 example improved

--- examples/example_pilatusPC12.py
+++ examples/example_pilatusPC12.py
@@ -39,31 +39,35 @@ m = 6  # number of panels (spanwise)
 # Left wing
 A = np.array([.414, -b/2])
 B = np.array([0, -1.03])
-leading_edges_coord = [A, B]
-chord_lengths = [1.24, c]
-
-pilatusPC12.add_geometry(leading_edges_coord, chord_lengths, n, m)
+leading_edges_coord_lw = [A, B]
+chord_lengths_lw = [1.24, c]
 
 # Right wing
 C = np.array([0, 1.03])
 D = np.array([0.414, b/2])
-leading_edges_coord = [C, D]
-chord_lengths = [c, 1.24]
+leading_edges_coord_rw = [C, D]
+chord_lengths_rw = [c, 1.24]
 
-pilatusPC12.add_geometry(leading_edges_coord, chord_lengths, n, m)
+## Horizontal stabilizer
+#A = np.array([8.284, -2.07])
+#B = np.array([7.87, 0])
+#C = np.array([8.284, 2.07])
+#leading_edges_coord_hs = [A, B, C]
+#chord_lengths_hs = [0.911, 1.33, 0.911]
 
-# Horizontal stabilizer
-# A = np.array([8.284, -2.07])
-# B = np.array([7.87, 0])
-# C = np.array([8.284, 2.07])
-# leading_edges_coord = [A, B, C]
-# chord_lengths = [0.911, 1.33, 0.911]
+pilatusPC12.add_geometry(leading_edges_coord_lw, chord_lengths_lw, n, m)
+pilatusPC12.add_geometry(leading_edges_coord_rw, chord_lengths_rw, n, m)
+#pilatusPC12.add_geometry(leading_edges_coord_hs, chord_lengths_hs, 4, 4)
 
-# pilatusPC12.check_mesh()
+pilatusPC12.check_mesh()
 
 # SIMULATION
 # Flight condition parameters
 V = 140.0
 alpha = np.deg2rad(0)
+
+print('\nResults for the following flight conditions:'
+      '\n    V = %4.1f m/s \n    alpha = %3.1f degrees'
+      % (V, np.rad2deg(alpha)))
 
 pilatusPC12.vlm(V, alpha, True)
