3ce407b363f093632cf4a174051a93146e5cdf91
aneqr <andres.quezada.reed@gmail.com>
2017-06-22 22:22:37 +0200

    generating the wing using the semi span. Example updated

--- examples/example_pilatusPC12.py
+++ examples/example_pilatusPC12.py
@@ -1,24 +1,22 @@
 """
-    This example shows how to use the PyVLM class in order
+    This example shows the use of the PyVLM class in order
     to generate the wing planform of a Pilatus PC12 airplane.
 
-    After defining the flight conditions (airspeed and AOA),
-    the geometry will be characterised using the following
-    nomenclature:
+    The left wing will be characterised using the following
+    nomenclature while the right wing will be created as the
+    specular image of the left wing:
 
-    Y  ^  D +--+
-       |   /    \
+    Y  ^   +----+
        |  /      \
        | /        \
        |/          \
-     C +------------+
+       +------------+
        +-------------------->
      B +------------+        X
         \          /
          \        /
           \      /
-           \    /
-          A +--+
+         A +----+
 """
 
 import numpy as np
@@ -29,42 +27,20 @@ from pyvlm.vlm import PyVLM
 
 pilatusPC12 = PyVLM()
 
-# GEOMETRY DEFINITION #
-# Parameters
-c = 2.15  # root chord length
-b = 16.28  # panel span length
-n = 8  # number of panels (chordwise)
-m = 6  # number of panels (spanwise)
+# Geometry parameters
+c, b = 2.15, 16.28  # root chord length, panel span length
+n, m = 8, 6  # number of panels (chordwise, spanwise)
 
 # Left wing
-A = np.array([.414, -b/2])
-B = np.array([0, -1.03])
-leading_edges_coord_lw = [A, B]
-chord_lengths_lw = [1.24, c]
+A, B = np.array([.414, -b/2]), np.array([0, -1.03])
+leading_edges_coord = [A, B]
+chord_lengths = [1.24, c]
 
-# Right wing
-C = np.array([0, 1.03])
-D = np.array([0.414, b/2])
-leading_edges_coord_rw = [C, D]
-chord_lengths_rw = [c, 1.24]
-
-## Horizontal stabilizer
-#A = np.array([8.284, -2.07])
-#B = np.array([7.87, 0])
-#C = np.array([8.284, 2.07])
-#leading_edges_coord_hs = [A, B, C]
-#chord_lengths_hs = [0.911, 1.33, 0.911]
-
-pilatusPC12.add_geometry(leading_edges_coord_lw, chord_lengths_lw, n, m)
-pilatusPC12.add_geometry(leading_edges_coord_rw, chord_lengths_rw, n, m)
-#pilatusPC12.add_geometry(leading_edges_coord_hs, chord_lengths_hs, 4, 4)
-
+pilatusPC12.add_wing(leading_edges_coord, chord_lengths, n, m)
 pilatusPC12.check_mesh()
 
-# SIMULATION
 # Flight condition parameters
-V = 140.0
-alpha = np.deg2rad(0)
+V, alpha = 140.0, np.deg2rad(0)
 
 print('\nResults for the following flight conditions:'
       '\n    V = %4.1f m/s \n    alpha = %3.1f degrees'
