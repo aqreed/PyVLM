12bfd3e07942cb6afa0d6404fa6dc0f5dde5510d
aqreed <andres.quezada.reed@gmail.com>
2017-10-07 18:19:53 +0200

    edited minor errors in docs

--- examples/example_pilatusPC12.py
+++ examples/example_pilatusPC12.py
@@ -24,17 +24,18 @@ pilatusPC12 = PyVLM()
 
 # Geometry parameters
 A = np.array([0, 1.03])  # root chord leading edge position
-B = np.array([.414, 8.14])  # root chord leading edge position
+B = np.array([.414, 8.14])  # tip chord leading edge position
 
-leading_edges_coord = [A, B]
-chord_lengths = [2.15, 1.24]  # root, tip
+leading_edges_position = [A, B]
+chord_length = [2.15, 1.24]  # root, tip
 
 n, m = 3, 4  # number of panels (chordwise, spanwise)
-pilatusPC12.add_wing(leading_edges_coord, chord_lengths, n, m)
+pilatusPC12.add_wing(leading_edges_position, chord_length, n, m)
 pilatusPC12.check_mesh()
 
 # Flight condition parameters
-V, alpha = 140.0, np.deg2rad(0)
+V = 140.0
+alpha = np.deg2rad(0)
 
 print('\nResults for the following flight conditions:'
       '\n    V = %4.1f m/s \n    alpha = %3.1f degrees'
