98dedad82d5620da6c58dfad80c5ac4a997714e4
aqreed <andres.quezada.reed@gmail.com>
2017-10-14 22:23:06 +0200

    added test for the PyVLM class

---
+++ src/pyvlm/tests/test_vlm.py
@@ -0,0 +1,55 @@
+"""
+    Unit tests of the Mesh class and its methods
+
+"""
+
+import pytest
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+from pyvlm.vlm import PyVLM
+
+
+def test_add_wing():
+    test_wing = PyVLM()
+
+    A = np.array([0, 0])
+    B = np.array([0, 1])
+
+    leading_edges_position = [A, B]
+    chord_length = [1, 1]
+
+    n, m = 1, 2
+    test_wing.add_wing(leading_edges_position, chord_length, n, m)
+
+    calculated_points = test_wing.Points
+    expected_points = [np.array([0, 0]), np.array([0, .5]), np.array([0, 1]),
+                       np.array([1, 0]), np.array([1, .5]), np.array([1, 1]),
+                       np.array([0, -1]), np.array([0, -.5]), np.array([0, 0]),
+                       np.array([1, -1]), np.array([1, -.5]), np.array([1, 0])]
+
+    assert_almost_equal(calculated_points, expected_points)
+
+
+def test_vlm():
+    test_vlm = PyVLM()
+
+    A = np.array([0, 0])
+    B = np.array([0, 1])
+
+    leading_edges_position = [A, B]
+    chord_length = [1, 1]
+
+    n, m = 1, 1
+    test_vlm.add_wing(leading_edges_position, chord_length, n, m)
+
+    V = 140.0
+    alpha = np.deg2rad(0)
+
+    test_vlm.vlm(V, alpha, False)
+
+    calculated_AIC = test_vlm.AIC
+    expected_AIC = np.array([[-0.768468, 0.1634183],
+                             [0.1634183, -0.768468]])
+
+    assert_almost_equal(calculated_AIC, expected_AIC)
