eee70dc1f9fcebd2f3c49d510e2275a48d9dfbc5
aqreed <andres.quezada.reed@gmail.com>
2019-09-24 16:38:01 +0200

    completed test coverage for geometry functions

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -8,7 +8,7 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 
 from vlm.geometry import (cross_prod, vect_dot, norm_dir_vect,
-                            dist_point2line, area_4points)
+                          dist_point2line, area_4points)
 
 
 def test_cross_prod():
@@ -16,7 +16,7 @@ def test_cross_prod():
     B = np.array([1, 1])
 
     calculated_vector = cross_prod(A, B)
-    expected_vector = 0.0*1.0 - 0.0*1.0
+    expected_vector = 0.0 * 1.0 - 0.0 * 1.0
 
     assert_almost_equal(calculated_vector, expected_vector)
 
@@ -26,7 +26,7 @@ def test_vect_dot():
     B = np.array([1, 1])
 
     calculated_vector = vect_dot(A, B)
-    expected_vector = 0.0*1.0 + 0.0*1.0
+    expected_vector = 0.0 * 1.0 + 0.0 * 1.0
 
     assert_almost_equal(calculated_vector, expected_vector)
 
@@ -36,7 +36,7 @@ def test_norm_dir_vect():
     B = np.array([1, 1])
 
     calculated_vector = norm_dir_vect(A, B)
-    expected_vector = np.sqrt(2)/2 * np.array([1, 1])
+    expected_vector = np.sqrt(2) / 2 * np.array([1, 1])
 
     assert_almost_equal(calculated_vector, expected_vector)
 
@@ -48,6 +48,15 @@ def test_dist_point2line():
 
     calculated_dist = dist_point2line(P, A, B)
     expected_dist = 1.0
+
+    assert_almost_equal(calculated_dist, expected_dist)
+
+    P = np.array([1, 0])
+    A = np.array([0, 0])
+    B = np.array([2, 0])
+
+    calculated_dist = dist_point2line(P, A, B)
+    expected_dist = 0.0
 
     assert_almost_equal(calculated_dist, expected_dist)
 
