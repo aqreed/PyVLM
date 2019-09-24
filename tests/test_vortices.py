765fd3bdfbd82718977aeaa94640db813bfd8c81
aqreed <andres.quezada.reed@gmail.com>
2019-09-24 16:56:11 +0200

    completed test coverage for vortices functions

--- tests/test_vortices.py
+++ tests/test_vortices.py
@@ -8,8 +8,8 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 
 from vlm.vortices import (vortex_position_in_panel,
-                            v_induced_by_horseshoe_vortex,
-                            v_induced_by_finite_vortex_line)
+                          v_induced_by_horseshoe_vortex,
+                          v_induced_by_finite_vortex_line)
 
 
 def test_vortex_position_in_panel():
@@ -35,6 +35,36 @@ def test_v_induced_by_horseshoe_vortex():
     expected_vel = -0.674191156, -0.6030149
 
     assert_almost_equal(calculated_vel, expected_vel)
+
+    # point P in between P1 and P2: vtotal == vtrail
+    P = np.array([0, 0.5])
+    P1 = np.array([0, 0])
+    P2 = np.array([0, 1])
+
+    vtotal = v_induced_by_horseshoe_vortex(P, P1, P2)[0]
+    vtrail = v_induced_by_horseshoe_vortex(P, P1, P2)[1]
+
+    assert_almost_equal(vtotal, vtrail)
+
+    # point P = P1: vbound = vtrail1 = 0, vtotal = vtrail = vtrail2
+    P = np.array([0, 0])
+    P1 = np.array([0, 0])
+    P2 = np.array([0, 1])
+
+    vtotal = v_induced_by_horseshoe_vortex(P, P1, P2)[0]
+    vtrail = v_induced_by_horseshoe_vortex(P, P1, P2)[1]
+
+    assert_almost_equal(vtotal, vtrail)
+
+    # point P = P2: vbound = vtrail2 = 0, vtotal = vtrail = vtrail1
+    P = np.array([0, 1])
+    P1 = np.array([0, 0])
+    P2 = np.array([0, 1])
+
+    vtotal = v_induced_by_horseshoe_vortex(P, P1, P2)[0]
+    vtrail = v_induced_by_horseshoe_vortex(P, P1, P2)[1]
+
+    assert_almost_equal(vtotal, vtrail)
 
 
 def test_v_induced_by_finite_vortex_line():
