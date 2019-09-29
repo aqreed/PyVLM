ae7cd4c5b0b881d722fd7b0399f25e49e312ed87
aqreed <andres.quezada.reed@gmail.com>
2019-09-29 20:04:30 +0200

    added Bertin&Smith example. Imported array from numpy directly for readability

--- tests/test_vlm.py
+++ tests/test_vlm.py
@@ -5,7 +5,7 @@
 
 import pytest
 import unittest as ut
-import numpy as np
+from numpy import array, deg2rad
 from numpy.testing import assert_almost_equal
 
 from vlm import PyVLM
@@ -18,35 +18,35 @@ class test_PyVLM(ut.TestCase):
     def test_add_surface(self):
         plane = PyVLM()
 
-        A = np.array([0, 0])
-        B = np.array([0, 1])
+        A = array([0, 0])
+        B = array([0, 1])
         leading_edges_position = [A, B]
         chord_length = [1, 1]
         n, m = 1, 2
         plane.add_surface(leading_edges_position, chord_length, n, m)
 
         calculated_points = plane.Points
-        expected_points = [np.array([0, 0]), np.array([0, .5]),
-                           np.array([0, 1]), np.array([1, 0]),
-                           np.array([1, .5]), np.array([1, 1]),
-                           np.array([0, -1]), np.array([0, -.5]),
-                           np.array([0, 0]), np.array([1, -1]),
-                           np.array([1, -.5]), np.array([1, 0])]
+        expected_points = [array([0, 0]), array([0, .5]),
+                           array([0, 1]), array([1, 0]),
+                           array([1, .5]), array([1, 1]),
+                           array([0, -1]), array([0, -.5]),
+                           array([0, 0]), array([1, -1]),
+                           array([1, -.5]), array([1, 0])]
         plane.reset()
         assert_almost_equal(calculated_points, expected_points)
 
     def test_add_surface_exceptions(self):
         plane = PyVLM()
-        le_pos = [np.array([0, 0])]
+        le_pos = [array([0, 0])]
         chrd_len = [1, 1]
         n, m = 3, 4
         self.assertRaises(ValueError, plane.add_surface,
                           le_pos, chrd_len, n, m)
 
         plane = PyVLM()
-        A = np.array([0, 0])
-        B = np.array([0, 1])
-        C = np.array([0, 1])
+        A = array([0, 0])
+        B = array([0, 1])
+        C = array([0, 1])
         le_pos = [A, B, C]
         chrd_len = [1, 1, 1]
         self.assertRaises(ValueError, plane.add_surface,
@@ -54,8 +54,8 @@ class test_PyVLM(ut.TestCase):
 
     def test_show_mesh(self):
         plane = PyVLM()
-        A = np.array([0, 0])
-        B = np.array([0, 1])
+        A = array([0, 0])
+        B = array([0, 1])
         le_pos = [A, B]
         chrd_len = [1, 1]
         n, m = 2, 2
@@ -65,26 +65,26 @@ class test_PyVLM(ut.TestCase):
     def test_vlm(self):
         plane = PyVLM()
 
-        A = np.array([0, 0])
-        B = np.array([0, 1])
+        A = array([0, 0])
+        B = array([0, 1])
         leading_edges_position = [A, B]
         chord_length = [1, 1]
         n, m = 1, 1
         plane.add_surface(leading_edges_position, chord_length, n, m)
 
-        alpha = np.deg2rad(0)
+        alpha = deg2rad(0)
         plane.vlm(alpha, True)
         calculated_AIC = plane.AIC
-        expected_AIC = np.array([[-0.768468, 0.1634183],
-                                 [0.1634183, -0.768468]])
+        expected_AIC = array([[-0.768468, 0.1634183],
+                             [0.1634183, -0.768468]])
 
         assert_almost_equal(calculated_AIC, expected_AIC)
 
     def test_aerodyn_forces_coeff(self):
         plane = PyVLM()
 
-        A = np.array([0, 0])
-        B = np.array([0, 1])
+        A = array([0, 0])
+        B = array([0, 1])
         leading_edges_position = [A, B]
         chord_length = [1, 1]
         n, m = 1, 1
@@ -93,3 +93,28 @@ class test_PyVLM(ut.TestCase):
         plane.vlm(alpha, True)
         plane.aerodyn_forces_coeff()
         # TODO: improve this test with real-life values
+
+
+class test_BertinSmith(ut.TestCase):
+    """
+    Tests for the PyVLM class based on example 7.2 from Bertin, J.J. and
+    Smith, M.L.,"Aerodynamics for Engineers", 1998
+    """
+    def test_add_surface(self):
+        plane = PyVLM()
+
+        A = array([0, 0])
+        B = array([0.5, 0.5])
+        leading_edges_position = [A, B]
+        chord_length = [.2, .2]
+        n, m = 1, 4
+        plane.add_surface(leading_edges_position, chord_length,
+                          n, m, mirror=False)
+
+        calculated_points = plane.Points
+        expected_points = [array([0, 0]), array([0.125, 0.125]),
+                           array([0.25, 0.25]), array([0.375, 0.375]),
+                           array([0.5, 0.5]), array([0.2, 0.0]),
+                           array([0.325, 0.125]), array([0.45, 0.25]),
+                           array([0.575, 0.375]), array([0.7, 0.5])]
+        assert_almost_equal(calculated_points, expected_points)
